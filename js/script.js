77cf7490df977352fc54c25ddb61b6a84aafaf5a
Jazmine Sanders <98546231+jsanders316@users.noreply.github.com>
2022-06-04 17:33:47 -0400

    Accept and validate player guesses

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // guessed letters //
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 // guess botton //
 const guessButton = document.querySelector(".guess");
 // guess letter input //
@@ -16,6 +16,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder = function (word) {
   const placeHolderLetters = [];
@@ -30,7 +31,34 @@ placeholder(word);
 
 guessButton.addEventListener("click", function (e) {
   e.preventDefault();
+  message.innerText = "";
   const guess = letterInput.value;
-  console.log(guess);
+  const goodGuess = validateInput(guess);
+  if (goodGuess) {
+    makeGuess(guess);
+  }
   letterInput.value = "";
 });
+
+const validateInput = function (input) {
+  const acceptedLetter = /[a-zA-Z]/;
+  if (input.length === 0) {
+    message.innerText = "Please enter a letter.";
+  } else if (input.length > 1) {
+    message.innerText = "Please enter a single letter.";
+  } else if (!input.match(acceptedLetter)) {
+    message.innerText = "Please enter a letter from A to Z";
+  } else {
+    return input;
+  }
+};
+
+const makeGuess = function (guess) {
+  guess = guess.toUpperCase();
+  if (guessedLetters.includes(guess)) {
+    message.innerText = "You already guessed that letter. Try again.";
+  } else {
+    guessedLetters.push(guess);
+    console.log(guessedLetters);
+  }
+};
